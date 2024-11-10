3d98ce03a2a6a0b741a7277e32954db0409cd133
Al Jannat A. Uthman <67391668+ThelilJannah@users.noreply.github.com>
2024-11-10 18:59:49 +0100

    Add files via upload

--- CC/Main.java
+++ CC/Main.java
@@ -1,1 +1,39 @@
+package CC;
 
+public class Main {
+    public void caesar(char[] c, char[] d, int max, int min, int key, int mode){
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] == ' ') {
+                d[i] = c[i];
+            }else{
+                int asc = 0;
+                if (mode == 1) {
+                    asc = c[i] + key;
+                    if (asc > max) {
+                        asc -= max;
+                        asc = asc + min - 1;
+                    }
+
+                }else if (mode == 2){
+                    asc = c[i] - key;
+                    if (asc < min) {
+                        asc += max;
+                        asc = asc - min + 1;
+                    }
+
+                    }
+                d[i] = (char) asc;
+
+            }
+
+        }
+
+        String caesared = java.lang.String.valueOf(d);
+        if (mode == 1) {
+            System.out.println("Cipher text(s): " + caesared);
+        }else{
+            System.out.println("Plain text(s): " + caesared);
+        }
+
+    }
+}
